662d4683bb852b55d83a2ffbbf9f9e6dc50d2dca
Dhruva Bhat <dhruvabhat24@gmail.com>
2024-02-07 06:34:17 +0530

    Create Sort Characters By Frequency.java

---
+++ February/Java/Sort Characters By Frequency/Sort Characters By Frequency.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> hm = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+        
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
+            (a, b) -> b.getValue() - a.getValue()
+        );
+        
+        pq.addAll(hm.entrySet());
+        
+        StringBuilder result = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Map.Entry<Character, Integer> entry = pq.poll();
+            result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        }
+        
+        return result.toString();
+    }
+}
+
